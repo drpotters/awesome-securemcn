4fd1e9bb2f13ecc9172eeb455253de3a516d6daa
drpotters <daverpotter2@gmail.com>
2023-12-12 20:19:46 -0800

    Aligned variables to rest of the project

--- gcp/gke/locals.tf
+++ gcp/gke/locals.tf
@@ -1,8 +1,10 @@
 locals {
-  project_id      = data.tfe_outputs.root.values.gcpProjectId
+  project_id      = nonsensitive(data.tfe_outputs.root.values.gcpProjectId)
   region          = data.tfe_outputs.root.values.gcpRegion
   network_name    = data.tfe_outputs.gcp-infra.values.network_name_outside
   subnet_name     = data.tfe_outputs.gcp-infra.values.subnet_name_outside
   gcp_cidr        = data.tfe_outputs.root.values.gcp_cidr
-  deployment_name = format("%s-%s-gke-cluster", data.tfe_outputs.root.values.projectPrefix, data.tfe_outputs.root.values.buildSuffix)
+  projectPrefix   = data.tfe_outputs.root.values.projectPrefix
+  buildSuffix     = data.tfe_outputs.root.values.buildSuffix
+  deployment_name = format("%s-%s-gke-cluster", local.projectPrefix, local.buildSuffix)
 }
